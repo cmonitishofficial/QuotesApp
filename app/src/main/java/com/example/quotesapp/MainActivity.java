d691ee47d466b4dfdcef25656942bfa1be0b7c5d
Nitish Kumar <niralanitrogensir@gmail.com>
2021-01-04 21:15:58 +0530

    This is the first commit only for upload quotesApp.

---
+++ app/src/main/java/com/example/quotesapp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.quotesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button share;
+    TextView quotes;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        share = findViewById(R.id.btnShare);
+        quotes = findViewById(R.id.tvQuotes);
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String shareTxt = quotes.getText().toString();
+                Intent intent=new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.setType("txt/plain");
+                intent.putExtra(Intent.EXTRA_TEXT,shareTxt);
+                startActivity(intent);
+            }
+        });
+    }
+}
